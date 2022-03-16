d0e85e3e770926b546d7a86f7443b3b024775993
rouxaeduc <rouxa.educ@laposte.net>
2022-03-16 11:42:15 +0100

    IHM liste déroulante, tests de différents layout

--- Fenetre.java
+++ Fenetre.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.Color;
 import java.awt.event.*;
 import java.awt.*;
+import java.awt.FlowLayout;
 
 
 public class Fenetre extends JFrame implements ActionListener{ // besoin de boutons pour le menu, le chox de la scène
@@ -20,15 +21,55 @@ public class Fenetre extends JFrame implements ActionListener{ // besoin de bout
         this.setLocation(100, 100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // panel
-        JPanel panelGlobal = new JPanel();
-        panelGlobal.setLayout(null);
+        JPanel panelGlobal = new JPanel(new BorderLayout());
         panelGlobal.setBounds(0,0, this.width, this.height); // panneau qui englobe tt la fenetre
-        panelGlobal.setBackground(Color.pink);
-        JPanel panelSelection = new JPanel(); // choix scène
-        panelGlobal.setBounds(0,0, this.width, this.height); // panneau qui englobe tt la fenetre
-        panelGlobal.setBackground(Color.pink);
+        panelGlobal.setBackground(Color.blue);
 
-        panelGlobal.add(panelSelection);
+
+        /*JPanel panelSelection = new JPanel(); // choix scène
+        panelSelection.setSize(200,400);
+        panelSelection.setBackground(Color.green);
+         */
+
+        JPanel panelBoutons = new JPanel();
+        panelBoutons.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        panelBoutons.setBackground(Color.yellow);
+
+        JComboBox listeVolume=new JComboBox();
+        listeVolume.addItem("--choix volume--");
+        listeVolume.addItem("Sphère");
+        listeVolume.addItem("Cube");
+        listeVolume.addItem("Cylindre");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx= 1; // place la case contenant listeVolume en x=1 (x=0, y=0: coin à gauche de l'objet)
+        c.gridy= 1;
+        c.gridwidth = 2;
+        panelBoutons.add(listeVolume, c);
+
+        c.anchor= GridBagConstraints.FIRST_LINE_START; // position a interieur de la case de la liste deroulante
+        // par défaut: Constraints en CENTER
+        JComboBox listeMatieres=new JComboBox();
+        listeMatieres.addItem("--choix matieres--");
+        listeMatieres.addItem("Bois");
+        c.fill = GridBagConstraints.HORIZONTAL; // important de mettre ces lignes après creation ComboBox en question
+        c.gridx= 1;
+        c.gridy= 2;
+        c.weighty= 1.0; // gere espace entre les menus deroulants
+        c.gridwidth = 2; // nb de colonnes que prend le composant de long (long= selon x)
+        c.ipady = 30; // augmente largeur de la liste déroulante (largeur= selon y)
+
+
+
+        panelBoutons.add(listeMatieres, c);
+
+        //panelSelection.add(panelBoutons);
+
+        //panelGlobal.add(panelSelection, BorderLayout.WEST);
+
+        panelGlobal.add(panelBoutons, BorderLayout.WEST);
+
         add(panelGlobal);
         this.setVisible(true);
     }
@@ -39,15 +80,5 @@ public class Fenetre extends JFrame implements ActionListener{ // besoin de bout
     public static void main (String[]args){
         Fenetre f = new Fenetre(" IHM", 1000, 500);
     }
-
 }
-
-
-
-
-
-
-// IHM
-
-
-
+// A faire: reduire espace entre les listes deroulantes
