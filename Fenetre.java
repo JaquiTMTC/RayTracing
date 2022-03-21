6558970e226ecc390d824a20589e389a89d5c935
rouxaeduc <rouxa.educ@laposte.net>
2022-03-21 09:47:50 +0100

    Ajustement de la taille du panel des boutons (bandeau à droite) + actions des premiers boutons

--- Fenetre.java
+++ Fenetre.java
@@ -7,10 +7,16 @@ import java.awt.*;
 import java.awt.FlowLayout;
 
 
-public class Fenetre extends JFrame implements ActionListener{ // besoin de boutons pour le menu, le chox de la scène
+public class Fenetre extends JFrame implements ActionListener{ // besoin de boutons pour le menu, le choix de la scène
 // taille fenêtre et attributs
     int width;
     int height;
+    // declaration des boutons en attribut qui seront utilises dans le ActionPerformed
+    JButton etapeSuiv = new JButton(" Passer étape suivante");
+
+    JButton effRendu = new JButton ("Effacer le rendu");
+    JComboBox listeVolume=new JComboBox();
+    JComboBox listeMatieres=new JComboBox();
 
     // constructeur
     public Fenetre(String nom, int w, int h) {
@@ -25,56 +31,88 @@ public class Fenetre extends JFrame implements ActionListener{ // besoin de bout
         panelGlobal.setBounds(0,0, this.width, this.height); // panneau qui englobe tt la fenetre
         panelGlobal.setBackground(Color.blue);
 
+        JPanel panelMenu = new JPanel(new BorderLayout());
+        panelMenu.setLayout(new GridBagLayout());
+        panelMenu.setBackground(Color.orange);
 
-        /*JPanel panelSelection = new JPanel(); // choix scène
-        panelSelection.setSize(200,400);
-        panelSelection.setBackground(Color.green);
-         */
+        JLabel labelMenu = new JLabel ("menu");
+        GridBagConstraints co = new GridBagConstraints();
+        co.fill = GridBagConstraints.CENTER;
+        co.ipady = 20;
+        panelMenu.add(labelMenu, co);
 
         JPanel panelBoutons = new JPanel();
         panelBoutons.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-
         panelBoutons.setBackground(Color.yellow);
 
-        JComboBox listeVolume=new JComboBox();
+
         listeVolume.addItem("--choix volume--");
         listeVolume.addItem("Sphère");
         listeVolume.addItem("Cube");
         listeVolume.addItem("Cylindre");
+        listeVolume.addActionListener(this);
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx= 1; // place la case contenant listeVolume en x=1 (x=0, y=0: coin à gauche de l'objet)
         c.gridy= 1;
         c.gridwidth = 2;
+        c.ipady=30;
+        c.insets=new Insets(10,10,10,10);
         panelBoutons.add(listeVolume, c);
 
-        c.anchor= GridBagConstraints.FIRST_LINE_START; // position a interieur de la case de la liste deroulante
         // par défaut: Constraints en CENTER
-        JComboBox listeMatieres=new JComboBox();
+
         listeMatieres.addItem("--choix matieres--");
         listeMatieres.addItem("Bois");
+        c.anchor= GridBagConstraints.FIRST_LINE_START; // position a interieur de la case de la liste deroulante
         c.fill = GridBagConstraints.HORIZONTAL; // important de mettre ces lignes après creation ComboBox en question
         c.gridx= 1;
         c.gridy= 2;
         c.weighty= 1.0; // gere espace entre les menus deroulants
         c.gridwidth = 2; // nb de colonnes que prend le composant de long (long= selon x)
         c.ipady = 30; // augmente largeur de la liste déroulante (largeur= selon y)
-
-
-
+        c.insets=new Insets(10,10,10,10);
         panelBoutons.add(listeMatieres, c);
 
-        //panelSelection.add(panelBoutons);
 
-        //panelGlobal.add(panelSelection, BorderLayout.WEST);
+        etapeSuiv.setBackground(Color.green);
+        etapeSuiv.addActionListener(this);
+        c.fill=GridBagConstraints.HORIZONTAL;
+        c.gridx= 1;
+        c.gridy= 3;
+        c.insets=new Insets(10,10,10,10);
+        panelBoutons.add(etapeSuiv, c);
 
+
+        effRendu.setBackground(Color.red);
+        effRendu.addActionListener(this);
+        c.fill=GridBagConstraints.HORIZONTAL;
+        c.gridx= 1;
+        c.gridy= 4;
+        c.insets=new Insets(10,10,10,10);
+        panelBoutons.add(effRendu, c);
+
+
+        panelGlobal.add(panelMenu, BorderLayout.NORTH);
         panelGlobal.add(panelBoutons, BorderLayout.WEST);
+
+
 
         add(panelGlobal);
         this.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
+        System.out.println("Clic");
+        if(e.getSource()==etapeSuiv){
+            System.out.println("Etape suivante");
+        }
+        if(e.getSource()== effRendu){
+            System.out.println(" Effacer");
+        }
+        if(listeVolume.getSelectedItem()== "Sphère" ){
+            System.out.println(" Sphere selectionnée");
+        }
 
     }
     public static void main (String[]args){
