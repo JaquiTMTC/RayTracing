621d3e532350663ea8136ef9afe822281fa0cb7f
Clementine <clementine.vigier74@yahoo.fr>
2022-04-04 10:56:17 +0200

    Lundi 4 avril: Fenetre uniquement avec setBounds plus propre

--- FenetreCoord.java
+++ FenetreCoord.java
@@ -8,15 +8,16 @@ import java.awt.Font;
 
 
 //public class FenetreCoord extends JFrame implements ActionListener{ // besoin de boutons pour le menu, le choix de la scène
-public class FenetreCoord extends JFrame{
+public class FenetreCoord extends JFrame implements ActionListener{
     // taille fenêtre et attributs
     int width;
     int height;
     // declaration des boutons en attribut qui seront utilises dans le ActionPerformed
-    JComboBox listeVolume=new JComboBox();
-    JComboBox listeMatieres=new JComboBox();
+    JComboBox listeVolume = new JComboBox();
+    JComboBox listeMatieres = new JComboBox();
     JButton etapeSuiv1 = new JButton("Passer étape suivante");
-    JButton effRendu = new JButton ("Effacer le rendu");
+    JButton effRendu = new JButton("Effacer le rendu");
+    JButton sortie = new JButton(" Sortie du programme");
     JPanel panelEtape2 = new JPanel();
     JButton testAffichage = new JButton("Test affichage");
     JLabel testRendu = new JLabel(" ");
@@ -25,25 +26,25 @@ public class FenetreCoord extends JFrame{
     // constructeur
     public FenetreCoord(String nom, int w, int h) {
         super(nom);
-        this.width= w;
-        this.height= h;
-        this.setSize(width,height);
+        this.width = w;
+        this.height = h;
+        this.setSize(width, height);
         this.setLocation(100, 100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // panel
         JPanel panelGlobal = new JPanel();
-        panelGlobal.setBounds(0,0, this.width, this.height); // panneau qui englobe tt la fenetre
+        panelGlobal.setBounds(0, 0, this.width, this.height); // panneau qui englobe tt la fenetre
         panelGlobal.setBackground(Color.blue);
         panelGlobal.setLayout(null);
 
         JPanel panelMenu = new JPanel();
-        panelMenu.setBounds(0,0, 300, 50);
+        panelMenu.setBounds(0, 0, 300, 50);
         panelMenu.setLayout(null);
         panelMenu.setBackground(Color.orange);
 
 
-        JLabel labelMenu = new JLabel ("Menu");
-        labelMenu.setBounds(125, 10, 50, 20 );
+        JLabel labelMenu = new JLabel("Menu");
+        labelMenu.setBounds(125, 10, 50, 20);
         panelMenu.add(labelMenu);
         panelGlobal.add(panelMenu);
 
@@ -52,14 +53,14 @@ public class FenetreCoord extends JFrame{
         panelEtapes.setVisible(true);
         panelEtapes.setBackground(Color.yellow);
         panelEtapes.setLayout(null);
-        panelEtapes.setBounds(0,50,300, 450);
+        panelEtapes.setBounds(0, 50, 300, 450);
         panelGlobal.add(panelEtapes);
 
         // Panel etape 1
         JPanel panelEtape1 = new JPanel();
         panelEtape1.setVisible(true);
         panelEtape1.setLayout(null);
-        panelEtape1.setBounds(0,0,300, 150);
+        panelEtape1.setBounds(0, 0, 300, 150);
         panelEtape1.setBackground(Color.red);
         // Listes deroulantes Etape 1
         listeVolume.setBounds(10, 10, 200, 30);
@@ -67,15 +68,15 @@ public class FenetreCoord extends JFrame{
         listeVolume.addItem("Sphère");
         listeVolume.addItem("Cube");
         listeVolume.addItem("Cylindre");
-        //listeVolume.addActionListener(this);
+        listeVolume.addActionListener(this);
         listeMatieres.setBounds(10, 45, 200, 30);
         listeMatieres.addItem("--Choix matière--");
         listeMatieres.addItem("Bois");
-        //listeMatieres.addActionListener(this);
+        listeMatieres.addActionListener(this);
 
         etapeSuiv1.setBounds(10, 90, 200, 30);
         etapeSuiv1.setBackground(Color.green);
-        //etapeSuiv1.addActionListener(this);
+        etapeSuiv1.addActionListener(this);
 
         panelEtape1.add(listeVolume);
         panelEtape1.add(listeMatieres);
@@ -85,37 +86,35 @@ public class FenetreCoord extends JFrame{
         JPanel panelEtape2 = new JPanel();
         panelEtape2.setVisible(true);
         panelEtape2.setLayout(null);
-        panelEtape2.setBounds(0,150,300, 150);
+        panelEtape2.setBounds(0, 150, 300, 150);
         panelEtape2.setBackground(Color.cyan);
 
         panelEtapes.add(panelEtape2);
-        // Bu=utons d'etape2
+        // Boutons d'etape2
 
-
-/*
-etapeSuiv.setBackground(Color.green);
-        etapeSuiv.addActionListener(this);
-        panelEtape1.add(etapeSuiv);
-
-        effRendu.setBackground(Color.red);
+        // PanelEtape3
+        JPanel panelEtape3 = new JPanel();
+        panelEtape3.setVisible(true);
+        panelEtape3.setLayout(null);
+        panelEtape3.setBackground(Color.magenta);
+        panelEtape3.setBounds(0, 300, 300, 150);
+        panelEtapes.add(panelEtape3);
+        //Boutons d'etape 3
+        effRendu.setBounds(20, 20, 200, 50);
+        sortie.setBounds(20, 80, 200, 50);
         effRendu.addActionListener(this);
-        panelEtape1.add(effRendu);
+        panelEtape3.add(effRendu);
+        panelEtape3.add(sortie);
 
-        panelEtape2.setVisible(true);
-        panelEtape2.setBackground(Color.red);
-        panelEtape2.add(new JLabel("Coucou, c'est étape 2"));
-        testAffichage.addActionListener(this);
-        panelEtape2.add(testAffichage);
-        panelGlobal.add(panelEtape2);
-
-        JPanel panelZoneAffichage=new JPanel();
-        panelZoneAffichage.setBounds(200, 0, 300, 300);
+        //PANEL AFFICHAGE RENDU
+        JPanel panelZoneAffichage = new JPanel();
+        panelZoneAffichage.setBounds(500, 100, 300, 300);
         //cons.fill=;
         panelZoneAffichage.setVisible(true);
 
-        JLabel labelVide = new JLabel ("Future zone de rendu                                        ////");
+        JLabel labelVide = new JLabel("Future zone de rendu ////");
         // Test de modif de police de caractère
-        Font police = new Font(" Times New Roman ",Font.BOLD,18);
+        Font police = new Font(" Calibri ", Font.BOLD, 18);
         labelVide.setFont(police);
         //labelVide.setText(" RENDU");
         panelZoneAffichage.add(labelVide);
@@ -124,26 +123,35 @@ etapeSuiv.setBackground(Color.green);
 
         panelZoneAffichage.setBackground(Color.gray);
         panelGlobal.add(panelZoneAffichage);
+
         //window = new Window(0);: test pour lier IHM/Rendu en mettant window en para
-*/
+
 
         add(panelGlobal);
         this.setVisible(true);
-    }
+    } // fin du constructeur
 
-   /* public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {
         System.out.println("Clic");
-        if(e.getSource()==etapeSuiv){
+        if (e.getSource() == etapeSuiv1) {
             System.out.println("Etape suivante");
         }
-        if(e.getSource()== effRendu){
+        if (e.getSource() == effRendu) {
             System.out.println(" Effacer");
         }
-        if(listeVolume.getSelectedItem()== "Sphère" ){
+        if (listeVolume.getSelectedItem() == "Sphère") {
             System.out.println("Sphère sélectionnée");
             //panelEtape2.setVisible(true);
         }
-        if(e.getSource()==testAffichage){
+        if (listeVolume.getSelectedItem() == "Cube") {
+            System.out.println("Cube sélectionné");
+            //panelEtape2.setVisible(true);
+        }
+        if (listeMatieres.getSelectedItem() == "Bois") {
+            System.out.println("Bois sélectionné");
+            //panelEtape2.setVisible(true);
+        }
+        if (e.getSource() == testAffichage) {
             System.out.println("Bouton test affichage cliqué");
             /*JFrame frame = new JFrame();
             Window window = new Window(0);
@@ -155,19 +163,21 @@ etapeSuiv.setBackground(Color.green);
 
             Timer timer = new Timer(10, window);
             //timer.start();
-
-
-            JLabel testRendu= new JLabel(new ImageIcon("./Crepe2.png"));
-            //panelZoneAffichage.add(testRendu);
-            testRendu.setVisible(true);
+            */
+            JLabel testAFF= new JLabel(new ImageIcon("./Crepe2.png"));
+            //panelZoneAffichage.add(testAFF);
+            testAFF.setVisible(true);
+            panelEtape2.add(testAFF);
         }
-        */
-
-
-    //}
-    public static void main (String[]args){ FenetreCoord f = new FenetreCoord(" IHM", 1000, 535);
 
     }
+
+
+        public static void main (String[]args){
+            FenetreCoord f = new FenetreCoord(" IHM", 1000, 535);
+
+        }
+
 }
 
 
